7b7485bf30faa267c259359eb898208ad6b7b0f3
Anh Tuan Le <63275793+tuanle273@users.noreply.github.com>
2021-06-14 10:10:28 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang=“en”>
+<head>
+<meta charset=”UTF-8”>
+<title> Welcome to PHP for cloud app!</title>
+</head>
+<body>
+<?php
+	$hello = "Hello"; 
+	$name = "Cloud computing class";
+	$text = $hello." ".$name;
+	echo "<h1> $text </h1>";
+	
+?>
+</body>
+</html>
